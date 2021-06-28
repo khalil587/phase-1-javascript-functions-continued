a09dbe081a3394ac555eae251be18181bd9e2f5e
Khalil Lindsay <lindsay.khalil@gmail.com>
2021-06-28 14:31:39 -0400

    commit tree 9c9406f6a37d4730a4879ed59582845df16b9119

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Your code here
+ function saturdayFun(activity="roller-skate"){
+    return(`This Saturday, I want to ${activity}!`);   
+} 
+saturdayFun("bathe my dog"); 
+saturdayFun();
+
+function mondayWork(job="go to the office"){
+   return(`This Monday, I will ${job}.`);
+
+}
+mondayWork();
+mondayWork("work from home");
+
+function wrapAdjective(b="*"){
+    function coder(a="special"){
+      return(`You are ${b}${a}${b}!`);
+   }
+   return coder
+}
